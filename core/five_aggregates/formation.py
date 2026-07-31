e800b854169e000d36b091a8c5707753d81b0019
q1z2q3-debug <q1z2q3@gmail.com>
2026-07-31 22:58:46 +0800

    Add five aggregates formation layer

---
+++ core/five_aggregates/formation.py
@@ -0,0 +1,9 @@
+"""TC-AGI Five Aggregates: Formation (行)
+
+Represents intention and action formation.
+"""
+
+
+class FormationLayer:
+    def plan(self, state):
+        return {"planned_state": state}
