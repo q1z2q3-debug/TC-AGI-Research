2bb09fa19d120a4e1eb346f3347dc66e09071025
q1z2q3-debug <q1z2q3@gmail.com>
2026-07-31 22:58:52 +0800

    Add five aggregates consciousness layer

---
+++ core/five_aggregates/consciousness.py
@@ -0,0 +1,9 @@
+"""TC-AGI Five Aggregates: Consciousness (识)
+
+Meta-cognitive awareness layer.
+"""
+
+
+class ConsciousnessLayer:
+    def reflect(self, state):
+        return {"observed_state": state, "reflection": True}
