7b7f09ac2987e1e8d87989bfc35a01206f6607fa
q1z2q3-debug <q1z2q3@gmail.com>
2026-08-01 07:53:39 +0800

    feat: initialize cognitive life kernel

---
+++ cognitive_life/kernel/__init__.py
@@ -0,0 +1,13 @@
+"""TC-AGI V0.9.8 Cognitive Life Kernel."""
+
+from .existence_kernel import ExistenceKernel
+from .axiom_engine import AxiomEngine
+from .cognitive_state import CognitiveState
+from .autonomous_agent import AutonomousAgent
+
+__all__ = [
+    "ExistenceKernel",
+    "AxiomEngine",
+    "CognitiveState",
+    "AutonomousAgent",
+]
