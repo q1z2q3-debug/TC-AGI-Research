54637ee05ae62b7869407d8aaa5edd9b44d3b84c
q1z2q3-debug <q1z2q3@gmail.com>
2026-08-01 07:45:23 +0800

    feat: initialize cognitive life architecture package

---
+++ cognitive_life/__init__.py
@@ -0,0 +1,7 @@
+"""TC-AGI V0.9.8 Autonomous Cognitive Life Architecture."""
+
+from .life_cycle_engine import LifeCycleEngine
+from .identity_os import IdentityOS
+from .dao_firmware import DaoFirmware
+
+__all__ = ["LifeCycleEngine", "IdentityOS", "DaoFirmware"]
