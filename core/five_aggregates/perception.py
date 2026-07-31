02dd4740c4acdeade00a72a5e96474885c86530c
q1z2q3-debug <q1z2q3@gmail.com>
2026-07-31 22:58:38 +0800

    Add five aggregates perception layer

---
+++ core/five_aggregates/perception.py
@@ -0,0 +1,9 @@
+"""TC-AGI Five Aggregates: Perception (想)
+
+Transforms observations into conceptual representations.
+"""
+
+
+class PerceptionLayer:
+    def transform(self, observation):
+        return {"concept": observation}
