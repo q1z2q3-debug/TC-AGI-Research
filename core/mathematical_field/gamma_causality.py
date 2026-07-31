fa45dcba3236c06298aed5414dfba3ff555bc3de
q1z2q3-debug <q1z2q3@gmail.com>
2026-07-31 23:07:35 +0800

    Add gamma causal complexity model

---
+++ core/mathematical_field/gamma_causality.py
@@ -0,0 +1,9 @@
+"""TC-AGI Mathematical Cognitive Field: Gamma Causality
+
+Represents accumulated causal complexity.
+"""
+
+
+class GammaCausality:
+    def weight(self, relations):
+        return sum(relations)
